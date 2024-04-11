ce5e9e6645f1d4dc1adf7bd9aed386fcced031fb
gus0660 <guillaumeroch3@gmail.com>
2024-04-11 15:01:35 +0200

    ok fin de 3eme et début de 4eme partie

--- exoLior.php
+++ exoLior.php
@@ -1,29 +1,5 @@
 <?php
-
-class Employe
-{
-    public $nom;
-    public $prenoms;
-    public $age;
-
-    // ce n'est pas une fonction mais une methode
-    public function presentation($nom, $prenoms, $age)
-    {
-        var_dump("Bonjour je m'appelle $nom $prenoms et j'ai $age ans"); {
-        }
-    }
-}
-// cette variable($employe1) est une instance, c'est un objet(new) de la "class Employe"
-$employe1 = new Employe();
-$employe1->nom = "roch";
-$employe1->prenoms = "Guillaume";
-$employe1->age = 25;
-
-$employe2 = new Employe();
-$employe2->nom = "pernin";
-$employe2->prenoms = "Magali";
-$employe2->age = 36;
-
+//                  1ERE PARTIE
 // $nom = "roch";
 // $prenom = "Guillaume";
 // $age = 25;
@@ -35,4 +11,90 @@ $employe2->age = 36;
 // }
 // presentation($nom, $prenom, $age);
 // presentation($nom2, $prenom2, $age2);
-$employe1->presentation($employe1->nom, $employe1->prenoms, $employe1->age);
+
+//                  2EME PARTIE
+// class Employe
+// {// en premier, on définit les propriétés
+// public $nom;
+// public $prenoms;
+// public $age;
+
+// ce n'est pas une fonction mais une methode car elle se situe dans une classe
+// public function presentation()
+// {
+// var_dump("Bonjour je m'appelle $this->nom $this->prenoms et j'ai $this->age ans"); {
+// }
+// }
+// }
+// cette variable($employe1) est une instance, c'est un objet(new) de la "class Employe"
+// $employe1 = new Employe();
+// $employe1->nom = "roch";
+// $employe1->prenoms = "Guillaume";
+// $employe1->age = 25;
+// 
+// $employe2 = new Employe();
+// $employe2->nom = "pernin";
+// $employe2->prenoms = "Magali";
+// $employe2->age = 36;
+// j'appelle la methode "presentation()" de la classe Employe en passant par l'objet $employe1
+// $employe1->presentation();
+
+//                          3EME PARTIE
+// class Employe
+// { // en premier, on définit les propriétés
+    // public $nom;
+    // public $prenoms;
+    // public $age;
+
+    // public function __construct($nom, $prenoms, $age)
+    // {
+        // $this->nom = $nom;
+        // $this->prenoms = $prenoms;
+        // $this->age = $age;
+        // var_dump("HELLO $this->nom $this->prenoms");
+    // }
+
+    // ce n'est pas une fonction mais une methode car elle se situe dans une classe
+    // public function presentation()
+    // {
+        // var_dump("Bonjour je m'appelle $this->nom $this->prenoms et j'ai $this->age ans"); {
+        // }
+    // }
+// }
+// cette variable($employe1) est une instance, c'est un objet(new) de la "class Employe"
+// $employe1 = new Employe("roch", "Guillaume", 25);
+
+// $employe2 = new Employe("pernin", "Magali", 36);
+// j'appelle la methode "presentation()" de la classe Employe en passant par l'objet $employe1
+// $employe1->presentation();
+// $employe2->presentation();
+
+//                          4EME PARTIE
+class Employe
+{ // en premier, on définit les propriétés
+    public $nom;
+    public $prenoms;
+    public $age;
+
+    public function __construct($nom, $prenoms, $age)
+    {
+        $this->nom = $nom;
+        $this->prenoms = $prenoms;
+        $this->age = $age;
+        var_dump("HELLO $this->nom $this->prenoms");
+    }
+
+    // ce n'est pas une fonction mais une methode car elle se situe dans une classe
+    public function presentation()
+    {
+        var_dump("Bonjour je m'appelle $this->nom $this->prenoms et j'ai $this->age ans"); {
+        }
+    }
+}
+// cette variable($employe1) est une instance, c'est un objet(new) de la "class Employe"
+$employe1 = new Employe("roch", "Guillaume", 25);
+
+$employe2 = new Employe("pernin", "Magali", 36);
+// j'appelle la methode "presentation()" de la classe Employe en passant par l'objet $employe1
+$employe1->presentation();
+$employe2->presentation();
