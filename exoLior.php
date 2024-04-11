a502d80f6cfcb1932ea02074f92b429fb75ac5b5
gus0660 <guillaumeroch3@gmail.com>
2024-04-11 13:59:57 +0200

    2ere partie ok

--- exoLior.php
+++ exoLior.php
@@ -1,22 +1,24 @@
 <?php
-$nom = "roch";
-$prenom = "Guillaume";
-$age = 25;
 
-$nom2 = "pernin";
-$prenom2 = "Magali";
-$age2 = 36;
-
-function presentation($nom, $prenom, $age){
-    var_dump("Bonjour je m'appelle $nom $prenom et j'ai $age ans");
+class employé
+{
+    public $nom;
+    public $prenoms;
+    public $age;
+    public function presentation($nom, $prenoms, $age)
+    {
+        var_dump("Bonjour je m'appelle $nom $prenoms et j'ai $age ans"); {
+        }
+    }
 }
+// $nom = "roch";
+// $prenom = "Guillaume";
+// $age = 25;
+// $nom2 = "pernin";
+// $prenom2 = "Magali";
+// $age2 = 36;
+// function presentation($nom, $prenom, $age){
+//     var_dump("Bonjour je m'appelle $nom $prenom et j'ai $age ans");
+// }
 presentation($nom, $prenom, $age);
 presentation($nom2, $prenom2, $age2);
-
-
-
-
-
-
-
-
