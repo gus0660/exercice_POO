ce691870ca666b27f323b5132cc829180083336f
gus0660 <guillaumeroch3@gmail.com>
2024-04-11 16:01:13 +0200

    ok pour 4eme partie

--- exoLior.php
+++ exoLior.php
@@ -70,31 +70,73 @@
 // $employe2->presentation();
 
 //                          4EME PARTIE
+// class Employe
+// {
+    // public $nom;
+    // public $prenoms;
+    // private $age;
+    // public function __construct($nom, $prenoms, $age)
+    // {
+        // $this->nom = $nom;
+        // $this->prenoms = $prenoms;
+        // $this->setAge($age);
+        // var_dump("HELLO $this->nom $this->prenoms");
+    // }
+    // public function setAge($age) {
+        // if(is_int($age) && $age >= 1 && $age <= 100){
+            // $this->age = $age;
+        // } else {
+            // throw new Exception("L'age d'un employe doit etre comprise entre 1 et 100 !");
+        // }
+    // }
+    // public function getAge() {
+        // return $this->age;
+    // }
+    // 
+    // public function presentation()
+    // {
+        // var_dump("Bonjour je m'appelle $this->nom $this->prenoms et j'ai $this->age ans"); {
+        // }
+    // }
+// }
+// $employe1 = new Employe("roch", "Guillaume", 25);
+// $employe2 = new Employe("pernin", "Magali", 36);
+// $employe1->setAge(54);
+// $employe1->presentation();
+// $employe2->presentation();
+
+//                          5EME PARTIE
 class Employe
-{ // en premier, on définit les propriétés
+{
     public $nom;
     public $prenoms;
-    public $age;
-
+    private $age;
     public function __construct($nom, $prenoms, $age)
     {
         $this->nom = $nom;
         $this->prenoms = $prenoms;
-        $this->age = $age;
+        $this->setAge($age);
         var_dump("HELLO $this->nom $this->prenoms");
     }
-
-    // ce n'est pas une fonction mais une methode car elle se situe dans une classe
+    public function setAge($age) {
+        if(is_int($age) && $age >= 1 && $age <= 100){
+            $this->age = $age;
+        } else {
+            throw new Exception("L'age d'un employe doit etre comprise entre 1 et 100 !");
+        }
+    }
+    public function getAge() {
+        return $this->age;
+    }
+    
     public function presentation()
     {
         var_dump("Bonjour je m'appelle $this->nom $this->prenoms et j'ai $this->age ans"); {
         }
     }
 }
-// cette variable($employe1) est une instance, c'est un objet(new) de la "class Employe"
 $employe1 = new Employe("roch", "Guillaume", 25);
-
 $employe2 = new Employe("pernin", "Magali", 36);
-// j'appelle la methode "presentation()" de la classe Employe en passant par l'objet $employe1
+$employe1->setAge(54);
 $employe1->presentation();
 $employe2->presentation();
