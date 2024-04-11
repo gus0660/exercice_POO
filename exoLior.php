62f2b40909f82402682d4682957674f6b8259a1f
gus0660 <guillaumeroch3@gmail.com>
2024-04-11 14:12:18 +0200

    3eme partie ok

--- exoLior.php
+++ exoLior.php
@@ -1,16 +1,29 @@
 <?php
 
-class employé
+class Employe
 {
     public $nom;
     public $prenoms;
     public $age;
+
+    // ce n'est pas une fonction mais une methode
     public function presentation($nom, $prenoms, $age)
     {
         var_dump("Bonjour je m'appelle $nom $prenoms et j'ai $age ans"); {
         }
     }
 }
+// cette variable($employe1) est une instance, c'est un objet(new) de la "class Employe"
+$employe1 = new Employe();
+$employe1->nom = "roch";
+$employe1->prenoms = "Guillaume";
+$employe1->age = 25;
+
+$employe2 = new Employe();
+$employe2->nom = "pernin";
+$employe2->prenoms = "Magali";
+$employe2->age = 36;
+
 // $nom = "roch";
 // $prenom = "Guillaume";
 // $age = 25;
@@ -20,5 +33,6 @@ class employé
 // function presentation($nom, $prenom, $age){
 //     var_dump("Bonjour je m'appelle $nom $prenom et j'ai $age ans");
 // }
-presentation($nom, $prenom, $age);
-presentation($nom2, $prenom2, $age2);
+// presentation($nom, $prenom, $age);
+// presentation($nom2, $prenom2, $age2);
+$employe1->presentation($employe1->nom, $employe1->prenoms, $employe1->age);
