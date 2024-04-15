c5aa21090327beaa0239d0d1e2aebee543e68f03
gus0660 <guillaumeroch3@gmail.com>
2024-04-15 10:06:26 +0200

    ok

--- persone.php
+++ persone.php
@@ -1,4 +1,11 @@
 <?php 
-class Persone{
-    
+class Personne{
+    public $nom;
+    public $prenoms;
+    public $age;
+
+    public function presentation(){
+        var_dump("Bonjour je m'appelle $this->nom $this->prenoms et j'ai $this->age ans");
+    }
+
 }
